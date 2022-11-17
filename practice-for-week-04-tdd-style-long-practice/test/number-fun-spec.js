4f3efd970ee29c94541a2333f09ab41d09166040
Zaineb Marediya <zmarediya@yahoo.com>
2022-11-17 17:53:54 -0600

    new changes

--- practice-for-week-04-tdd-style-long-practice/test/number-fun-spec.js
+++ practice-for-week-04-tdd-style-long-practice/test/number-fun-spec.js
@@ -1,1 +1,22 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+
+const { returnsThree, reciprocal } = require("../problems/number-fun");
+
+describe("returnsThree()", () => {
+    it("should return three", () => {
+        let result = returnsThree();
+        expect(result).to.eql(3);
+    })
+})
+
+describe("reciprocal(n)", () => {
+    it("should return the reciprocal of n", () => {
+        const x = 1000;
+
+        let resultX = reciprocal(x);
+
+        expect(resultX).to.eql(.001);
+
+    })
+})
